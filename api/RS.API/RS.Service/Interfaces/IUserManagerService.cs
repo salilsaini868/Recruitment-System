3fc1f01bb15c9b609ea67931e5d3c19d63ca1cd2
karthikkharvi <karthik.kharvi@evry.com>
2018-01-03 18:26:15 +0530

    Crud operation for user

--- api/RS.API/RS.Service/Interfaces/IUserManagerService.cs
+++ api/RS.API/RS.Service/Interfaces/IUserManagerService.cs
@@ -1,12 +1,20 @@
-﻿using RS.Common.Enums;
+﻿using RS.Common.CommonData;
+using RS.Common.Enums;
 using RS.ViewModel.User;
 using System;
 
 namespace RS.Service.Interfaces
 {
-    public interface IUserService
+    public interface IUserManagerService
     {
         UserViewModel LoginUser(string Email, string Password);
 
+        IResult createUser(UserViewModel user);
+
+        IResult updateUser(UserViewModel user);
+
+        IResult GetAllUser();
+
+        IResult GetUserById(int id);
     }
 }
--- api/RS.API/RS.Service/Logic/UserManagerService.cs
+++ api/RS.API/RS.Service/Logic/UserManagerService.cs
@@ -10,7 +10,7 @@ using RS.Entity.Models;
 
 namespace RS.Service.Logic
 {
-    public class UserManagerService : IUserService
+    public class UserManagerService : IUserManagerService
     {
         #region Global Variables
         private readonly IUserRepository _userRepository;
@@ -18,6 +18,50 @@ namespace RS.Service.Logic
         public UserManagerService(IUserRepository userRepository)
         {
             this._userRepository = userRepository;
+        }
+
+        public IResult createUser(UserViewModel user)
+        {
+            var result = new Result
+            {
+                Operation = Operation.Create,
+                Status = Status.Success
+            };
+            try
+            {
+                var userModel = new Users();
+                userModel.MapFromViewModel(user);
+                var duplicateUser = _userRepository.GetFirstOrDefault(x => x.UserName == user.UserName || x.Email == user.Email);
+                if (duplicateUser != null)
+                {
+                    result.Status = Status.Fail;
+                    result.Message = SkillStatusNotification.DuplicateSkill;
+                    result.Body = null;
+                }
+                else
+                {
+                    _userRepository.Create(userModel);
+                    _userRepository.SaveChanges();
+                    result.Body = userModel;
+                }
+            }
+            catch (Exception e)
+            {
+                result.Message = e.Message;
+                result.Status = Status.Fail;
+            }
+            return result;
+
+        }
+
+        public IResult GetAllUser()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult GetUserById(int id)
+        {
+            throw new NotImplementedException();
         }
         #region private members
 
@@ -31,6 +75,11 @@ namespace RS.Service.Logic
             UserViewModel userView = new UserViewModel();
             userView.MapFromModel(user, "UserName;Password" );
             return userView;
+        }
+
+        public IResult updateUser(UserViewModel user)
+        {
+            throw new NotImplementedException();
         }
 
         #endregion
