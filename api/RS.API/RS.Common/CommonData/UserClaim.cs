e0303483bcce7dba40a98b86b4fd21301ba16dea
salilsaini868 <m613210802>
2018-01-04 15:21:29 +0530

    Implementation of Audit column mapping

---
+++ api/RS.API/RS.Common/CommonData/UserClaim.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace RS.Common.CommonData
+{
+    public class UserClaim
+    {
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Role { get; set; }
+
+        public Guid UserId { get; set; }
+
+        public int RoleId { get; set; }
+    }
+}
