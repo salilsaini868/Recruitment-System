9328728d38c151b09f34eb3ac2d01464ed01cfe6
salilsaini868 <m613210802>
2018-01-23 11:27:04 +0530

    RHPM_WF01 : methods for workflow

--- api/RS.API/RS.Service/Interfaces/IApprovalManagerService.cs
+++ api/RS.API/RS.Service/Interfaces/IApprovalManagerService.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using RS.Common.CommonData;
 
 namespace RS.Service.Interfaces
 {
     public interface IApprovalManagerService
     {
+        IResult GetApprovalEvents(int approvalId);
     }
 }
