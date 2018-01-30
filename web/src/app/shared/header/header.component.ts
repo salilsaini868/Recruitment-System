3c6ab0e7b22427c44a40cb9c70b71535a081c486
vipan76 <ilovesoccer@88>
2018-01-30 16:58:38 +0530

    Dashboard UI screen with toggle icon

--- web/src/app/shared/header/header.component.ts
+++ web/src/app/shared/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AppConstants } from '../constant/constant.variable';
+import * as $ from 'jquery/dist/jquery.min.js'; 
 
 @Component({
     selector: 'app-header',
@@ -17,5 +18,17 @@ export class HeaderComponent implements OnInit {
         localStorage.removeItem(AppConstants.AuthToken);
         localStorage.clear();
         this.router.navigate(['login']);
+    }
+
+    toggleLeft(){
+        if(!$('.side-bar-main').hasClass("side-bar")) {
+            $('.side-bar-main').toggleClass("side-left", 'slow');
+        } 
+
+        if(!$('div').hasClass(".content-wrapper")) {
+            $('div.content-wrapper').toggleClass("wrap-left", 'slow');
+        } 
+
+        
     }
 }
--- web/src/app/shared/leftSideBar/leftSideBar.component.ts
+++ web/src/app/shared/leftSideBar/leftSideBar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-declare var jQuery: any; 
+
 
 @Component({
     selector: 'left-sideBar',
@@ -11,14 +11,6 @@ export class LeftSideBarComponent implements OnInit {
     isToggle : boolean = true;
     constructor() {}
     ngOnInit(): void {
-    }
-
-    toggleLeft(){
-        if(!jQuery('.side-bar-main').hasClass("side-bar")) {
-            jQuery('.side-bar-main').toggleClass("side-left");
-            jQuery('.side-bar-main').toggleClass("wrap-gap");
-        } 
-        
     }
 
 } 
