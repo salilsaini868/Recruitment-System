2fcbf949a28bb67dd9f11e1c6644058f1dc39182
bharath <bharathkeerthi.shivalingaiah@evry.com>
2018-02-08 14:37:08 +0530

    review changes done

--- web/src/app/Login/shared/login.serviceApp.ts
+++ web/src/app/Login/shared/login.serviceApp.ts
@@ -3,22 +3,18 @@ import { Headers, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/Rx';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
-import { LoginService, RoleService } from '../../webapi/services';
+import { LoginService } from '../../webapi/services';
 import { UserService } from '../../webapi/services';
 
 @Injectable()
 export class LoginServiceApp {
 
-    constructor(private http: HttpClient, private apiLoginService: LoginService, private apiForgotpasswordService: UserService, private apiRoleService: RoleService) { }
+    constructor(private http: HttpClient, private apiLoginService: LoginService) { }
 
     userLogin(userLoginModel): Observable<any> {
         return this.apiLoginService.ApiLoginLoginUserPost(userLoginModel).map(x => (x));
     }
-
-    getUser(): Observable<any> {
-        return this.apiRoleService.ApiRoleGetAllRoleGet().map(x => (x));
-    }
     userForgotpassword(userForgotpasswordModel): Observable<any> {
-        return ;
+        return;
     }
 }
