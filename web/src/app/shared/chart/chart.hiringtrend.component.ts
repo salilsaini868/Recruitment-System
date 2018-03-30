23a5eb4e9222c03421bcc6ddff6d1f4ecea0e9ad
salilsaini868 <m613210802>
2018-03-30 18:10:43 +0530

    RHPM_OT11: chart feature

--- web/src/app/shared/chart/chart.hiringtrend.component.ts
+++ web/src/app/shared/chart/chart.hiringtrend.component.ts
@@ -43,6 +43,9 @@ export class HiringTrendChartComponent implements OnInit, AfterContentInit, OnDe
       credits: {
         text: 'Salil Saini'
       },
+      exporting: {
+        enabled: true
+      },
       tooltip: {
         headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
         pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
